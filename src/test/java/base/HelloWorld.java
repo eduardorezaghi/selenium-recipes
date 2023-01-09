fae22d6ebbd8bff6d9b8f56d56749b901580d9b1
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-01-09 08:05:45 -0300

    Add basic examples and .vscode folder

---
+++ src/test/java/base/HelloWorld.java
@@ -0,0 +1,40 @@
+package base;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import static org.assertj.core.api.Assertions.*;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HelloWorld {
+
+    // Empty WebDriver instance before initiating tests
+    static WebDriver driver;
+    @BeforeAll
+    static void setup() {
+        // Use Boní Garcia WebDriverManager to initiate Selenium Webdriver
+        driver = WebDriverManager.chromedriver().create();
+    }
+    
+    @Test
+    void fillHelloWorldForm() {
+        driver.get("https://testpages.herokuapp.com/styled/basic-html-form-test.html");
+        
+        WebElement textArea = driver.findElement(By.name("comments"));
+        assertThat(textArea.getText()).isEqualTo("Comments...");
+
+        textArea.clear();
+        textArea.sendKeys("Hello, World!");
+        assertThat(textArea.getText()).isEqualTo("Hello, World!");
+    }
+
+    @AfterAll
+    static void teardown() {
+        // Use Boní Garcia WebDriverManager to initiate Selenium Webdriver
+        driver.quit();
+    }
+}
